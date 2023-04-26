ec08d285eef0d3b292d4de31239cf5e9e3132f5f
Seetharaman Sundar <60291990+SeetharamanSundar@users.noreply.github.com>
2023-04-26 23:10:56 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,89 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "To Save the files with .py in this language"
+    },
+    {
+        word: "kodaikanal",
+        hint: "Queen Of Hills"
+    },
+    {
+        word: "corn",
+        hint: "Theatre break time"
+    },
+    {
+        word: "aim",
+        hint: "Ambition another term"
+    },
+    {
+        word: "venus",
+        hint: "Hottest planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "Kind of jewel, women like!"
+    },
+    {
+        word: "amazon",
+        hint: "online shopping site"
+    },
+    {
+        word: "HTML",
+        hint: "Frontend language"
+    },
+    {
+        word: "CSS",
+        hint: "Design for Front-end"
+    },
+    
+    {
+        word: "bugs",
+        hint: "Error term in programming"
+    },
+    {
+        word: "avatar",
+        hint: "epic science fiction film"
+    },
+    {
+        word: "gif",
+        hint: "a file format for image"
+    },
+    {
+        word: "happiness",
+        hint: "related to the mind"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "related to a indoor game"
+    },
+    {
+        word: "viber",
+        hint: "Helps to vibe the rain water while driving inside a car "
+    },
+    {
+        word: "github",
+        hint: "Repository platform"
+    },
+    
+    {
+        word: "mobile",
+        hint: "an electronic device used to call the person at anytime and everywhere"
+    },
+    {
+        word: "Cpu",
+        hint: "computer component"
+    },
+    
+]
